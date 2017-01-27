0f0f9573e8900e76d554cfe570aebcd40398e4c2
mpuhwezimana <mpuhwearsene.com>
2017-01-27 15:24:33 +0200

    Work in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,9 @@ class ProductsController < ApplicationController
     price = params[:price]
     image = params[:image]
     product = Product.new({name: name, description: description, price: price, image: image  })
-    product.save    
+    product.save
+    redirect_to "/products/#{product.id}" 
+    flash[:success]= "Product created"
   end
 
   def edit
@@ -25,16 +27,19 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.description = params[:description]
-    @product.price = params[:price]
-    @product.image = params[:image]
-    @product.save
-  end
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name]
+    product.description = params[:description]
+    product.price = params[:price]
+    product.image = params[:image]
+    product.save
+    redirect_to "/products/#{product.id}"
+    flash[:info]= "Product updated"   end
 
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
+    redirect_to "/products"
+    flash[:warning]= "Product destroyed"
   end
 end
