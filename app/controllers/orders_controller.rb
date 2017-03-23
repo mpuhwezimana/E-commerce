2e667d14718214f67d1e95ea42224c32c5dc32b7
mpuhwezimana <mpuhwearsene.com>
2017-03-23 18:31:57 +0200

    End of the day

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
+
   def show
     @order = Order.find_by(id: params[:id])
   end
-
-  def create
-    @product  = Product.find_by(id: params[:product_id])
-    quantity = params[:quantity].to_i
-    subtotal = quantity * product.price
-    tax = quantity * product.tax
-    total = subtotal + tax
-
-    @order = Order.new(quantity: quantity, user_id: current_user.id, product_id: product.id, subtotal: subtotal, total: total)
-    if order.save
-     flash[:success] = "Order Created"
-    redirect_to '/orders/#{order.id}'
-    else
-    flash[:danger] = "Order Not Created"
-    redirect_to '/products/#{product.id}'
-    end
- end
 
   def update
     order = Order.find_by(id: params[:id])
@@ -36,4 +20,5 @@ class OrdersController < ApplicationController
     order.save
     redirect_to "/orders/#{order.id}"
   end
+
 end
